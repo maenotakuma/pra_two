334fcec78402cba3628d1c25e5554076c035d55b
maenotakuma <front858@gmail.com>
2020-08-10 09:11:09 +0900

    [add]rails-i18nの導入

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module PraTwo
       g.test_framework    false
     end
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
   end
 end
